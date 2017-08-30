47499efddbf285b85267efb6f74c8c6e45a55e7d
klootviool1 <31472459+klootviool1@users.noreply.github.com>
2017-08-30 13:12:39 +0200

    Update HalloRocDev.java

--- src/h01/HalloRocDev.java
+++ src/h01/HalloRocDev.java
@@ -6,6 +6,6 @@ package h01;
 public class HalloRocDev {
 
     public static void main(String[] args) {
-        System.out.println("Eerste aanpassing");
+        System.out.println("Kleine Aanpassing");
     }
 }
