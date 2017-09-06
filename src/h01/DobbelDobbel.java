0befa05fe61d033cdecf1f21ab5af37bd1333aba
Nikhil <nikskashyap1206@gmail.com>
2017-09-06 14:00:25 +0200

    8e commit

---
+++ src/h01/DobbelDobbel.java
@@ -0,0 +1,25 @@
+package h01;
+
+
+import java.applet.Applet;
+import java.awt.*;
+
+public class DobbelDobbel extends Applet {
+
+    public void init() {
+    }
+
+    //Dobbelsteen
+    public void paint(Graphics g) {
+        setBackground(Color.lightGray);
+
+        g.setColor(Color.white);
+        g.fillRoundRect(500, 300, 350, 350, 20, 20);
+        g.setColor(Color.black);
+        g.fillOval(560,360, 50,50);
+        g.fillOval(560, 550, 50,50);
+        g.fillOval(740, 550, 50, 50);
+        g.fillOval(740, 360, 50, 50);
+
+    }
+}
