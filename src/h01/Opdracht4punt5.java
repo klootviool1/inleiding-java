bd9bf36940c90f7cb99df29dc082d1ba370f02fb
Nikhil <nikskashyap1206@gmail.com>
2017-09-06 13:06:57 +0200

    6e commit

---
+++ src/h01/Opdracht4punt5.java
@@ -0,0 +1,21 @@
+package h01;
+
+
+    import java.awt.*;
+import java.applet.*;
+
+//Ellips
+
+    public class Opdracht4punt5 extends Applet {
+
+        public void init() {
+        }
+        public void paint(Graphics g) {
+            setBackground(Color.blue);
+            g.setColor(Color.yellow);
+            g.fillArc(600,200, 200, 400, 0, 360);
+
+    }
+    }
+
+
