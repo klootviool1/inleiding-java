8da2cd7669fcd2fef5681357ca473ed03c7240e1
Nikhil <nikskashyap1206@gmail.com>
2017-09-07 18:47:47 +0200

    Show helemaal af

--- src/h01/Show.java
+++ src/h01/Show.java
@@ -23,8 +23,17 @@ public class Show extends Applet {
         g.drawRect(70, 350, 30, 50);
         g.drawOval(80, 135, 40, 40);
         g.drawLine(50, 400, 200, 400);
+        g.drawLine(80, 155, 120, 155);
+        g.drawLine(100, 175, 100, 135);
+        g.drawOval(90, 380, 5, 5);
+        g.drawRect(130, 350, 50, 30);
+        g.drawRect(150, 250, 30, 40);
+        g.drawRect(70, 250, 30, 40);
 
-        //Vlaggetje
+
+
+
+     //Vlaggetje
         g.setColor(Color.black);
         g.drawLine(500, 200, 500, 600);
         g.drawLine(500, 200, 800, 200);
@@ -36,10 +45,10 @@ public class Show extends Applet {
         g.fillRect(500, 66, 300, 66);
 
         //Triangle
-        g.setColor(Color.green);
-        g.drawLine(130, 130,210,35);
-        g.drawLine(290, 130,210, 35);
-        g.drawLine(130,130, 290, 130);
+        g.setColor(Color.BLACK);
+        g.drawLine(250, 250,330,155);
+        g.drawLine(410, 250,330, 155);
+        g.drawLine(250,250, 410, 250);
 
         
 
