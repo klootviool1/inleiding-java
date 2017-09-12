54aa0487f7499f6e9c3fc575473070ab95d8f397
Nikhil <nikskashyap1206@gmail.com>
2017-09-12 09:33:03 +0200

    Praktijk Opdracht H5

--- src/h01/PraktijkOpdracht.java
+++ src/h01/PraktijkOpdracht.java
@@ -5,34 +5,47 @@ package h01;
 import java.applet.*;
 
     public class PraktijkOpdracht extends Applet {
+        //declaratie
+
+        Color opvulkleur;
+        Color lijnkleur;
+        int breedte;
+        int hoogte;
+
 
         public void init() {
+            //initialisatie
+
+            opvulkleur = Color.MAGENTA;
+            lijnkleur = Color.BLACK;
+            breedte = 200;
+            hoogte = 100;
         }
 
         public void paint(Graphics g) {
 
             //figuren
 
-            g.setColor(Color.red);
+            g.setColor(lijnkleur);
             g.drawLine(20, 30, 100, 30);
             g.drawString("Lijn", 20, 20);
-            g.setColor(Color.ORANGE);
+            g.setColor(lijnkleur);
             g.drawRect(20, 70, 100, 60);
             g.drawString("Rectangle", 20, 60);
-            g.setColor(Color.darkGray);
+            g.setColor(lijnkleur);
             g.drawRoundRect(20, 170, 100, 60, 20, 20);
             g.drawString("RoundRectangle", 20, 160);
-            g.setColor(Color.green);
+            g.setColor(opvulkleur);
             g.fillOval(20, 240, 100, 60);
-            g.setColor(Color.blue);
+            g.setColor(opvulkleur);
             g.fillRect(20, 320, 100, 60);
-            g.setColor(Color.BLACK);
+            g.setColor(lijnkleur);
             g.drawOval(20, 320, 100, 60);
-            g.setColor(Color.black);
+            g.setColor(lijnkleur);
             g.drawOval(20, 400, 100, 60);
-            g.setColor(Color.cyan);
+            g.setColor(opvulkleur);
             g.fillArc(20, 398, 100, 60, 45, 90);
-            g.setColor(Color.pink);
+            g.setColor(lijnkleur);
             g.drawOval(20, 490, 100, 100);
 
 
