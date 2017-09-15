722d441d9543a5ca7c306d2e260c9a572009bca1
Nikhil <nikskashyap1206@gmail.com>
2017-09-15 12:52:28 +0200

    Praktijk Opdracht H5

--- src/h01/JavaOpdrachten.java
+++ src/h01/JavaOpdrachten.java
@@ -6,14 +6,22 @@ package h01;
 
     public class JavaOpdrachten extends Applet {
 
+        Color invulkleur;
+        Color lijnkleur;
+        int breedte;
+        int hoogte1;
+
         public void init() {
+
+            breedte = 150;
+            hoogte1 = 140;
         }
 
         public void paint(Graphics g) {
 
             //Indicatie
             g.drawLine(190, 610, 190, 200);
-            g.drawLine(190, 610, 700, 610);
+            g.drawLine(190, 610, 760, 610);
 
             //Namen
             g.setColor(Color.black);
@@ -23,17 +31,24 @@ package h01;
 
             //Diagram
             g.setColor(Color.pink);
-            g.fillRect(200, 530, 80,80);
+            g.fillRect(200, 470, 150, hoogte1);
             g.setColor(Color.GREEN);
-            g.fillRect(400, 410, 80, 200);
+            g.fillRect(400, 240, 150, 370);
             g.setColor(Color.blue);
-            g.fillRect(600, 450, 80, 160);
+            g.fillRect(600, 320, 150, 290);
 
             //gewichten
             g.setColor(Color.BLACK);
-            g.drawString("40", 150, 550);
-            g.drawString("80", 150, 450);
-            g.drawString("100", 150, 400);
+            g.drawString("40", 150, 480);
+            g.drawString("80", 150, 320);
+            g.drawString("100", 150, 240);
+            g.drawString("10", 150, 600);
+            g.drawString("20", 150, 560);
+            g.drawString("30", 150, 520);
+            g.drawString("50", 150, 440);
+            g.drawString("60", 150, 400);
+            g.drawString("70", 150, 360);
+            g.drawString("90", 150, 280);
 
         }
     }
