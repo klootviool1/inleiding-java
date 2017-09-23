5ad7b99772dcacd12a49d811a97530bda3c818be
Nikhil <nikskashyap1206@gmail.com>
2017-09-23 12:37:01 +0200

    Opdracht 8.2

--- src/h08/Hoofdstuk8_2.java
+++ src/h08/Hoofdstuk8_2.java
@@ -1,36 +1,70 @@
 package h08;
 
-//8.7
+        import java.awt.*;
+        import java.applet.*;
+        import java.awt.event.*;
 
-import java.awt.*;
-import java.applet.*;
-import java.awt.event.*;
 
-    public class Hoofdstuk8_2 extends Applet {
-        TextField tekstvak;
-        Label label;
-        double getal;
+public class Hoofdstuk8_2 extends Applet {
+    Button knop;
+    Button knop2;
+    Button knop3;
+    Button knop4;
+    String schermtekst;
+    Label label;
+    String M;
+    String V;
+    String PV;
+    String ML;
+    int a;
+    int b;
+    int c;
+    int d;
+    String Totaal;
+    int totaal;
 
-        public void init() {
-            tekstvak = new TextField("", 20);
-            label = new Label("Type een getal");
-            tekstvak.addActionListener( new TekstvakListener() );
-            add(label);
-            add(tekstvak);
-        }
 
-        public void paint(Graphics g) {
-            g.drawString("Het getal is " + getal, 50, 60 );
-        }
+    public void init() {
+        schermtekst = " ";
+        knop = new Button("Ok");
+        KnopListener kl = new KnopListener();
+        knop.addActionListener(kl);
+        add(knop);
+        knop2 = new Button("Ok2");
+        add(knop2);
+        knop3 = new Button("Ok3");
+        kl = new KnopListener();
+        add(knop3);
+        knop4 = new Button ("Ok4");
+        kl = new KnopListener();
+        add(knop4);
+        label = new Label("");
+        add(label);
+        a = 50;
+        b = 50;
+        c = 75;
+        d = 75;
+        totaal = (a + b + c + d);
 
-        class TekstvakListener implements ActionListener {
-            public void actionPerformed(ActionEvent e) {
-                String s = tekstvak.getText();
-                getal = Double.parseDouble( s );
-                //Je kan deze regels ook samenvoegen
-                //getal = Double.parseDouble(tekstvak.getText());
-                repaint();
-            }
+    }
+
+    public void paint(Graphics g) {
+        g.drawString(M, 70, 70);
+        g.drawString(V, 70, 90);
+        g.drawString(PV,70, 110);
+        g.drawString(ML,70, 130);
+        g.drawString(Totaal, 70, 150);
+    }
+
+    class KnopListener implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            M = "Mannen : " + a;
+            V = "Vrouwen : " + b;
+            PV = "Potentiële vrouwen : " + c;
+            ML = "Mannelijke leerlingen : " + d;
+            Totaal = "totaal : " + totaal;
+            repaint();
         }
     }
 
+}
