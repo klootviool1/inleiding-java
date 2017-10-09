80858f3231648fdb3007bec58c17d8c7d5d09248
Nikhil <nikskashyap1206@gmail.com>
2017-10-09 09:43:48 +0200

    H11 opdracht 2

---
+++ src/h11/Hoofdstuk11_2.java
@@ -0,0 +1,27 @@
+package h11;
+
+import java.applet.Applet;
+import java.awt.*;
+
+public class Hoofdstuk11_2 extends Applet {
+
+    public void init() {
+
+
+    }
+    public void paint(Graphics g) {
+        setBackground(Color.BLACK);
+        g.setColor(Color.yellow);
+        g.drawString("10", 50, 50);
+        g.drawString("11", 80, 50);
+        g.drawString("12", 110, 50);
+        g.drawString("13", 140, 50);
+        g.drawString("14", 170, 50);
+        g.drawString("15", 200, 50);
+        g.drawString("16", 230, 50);
+        g.drawString("17", 260, 50);
+        g.drawString("18", 290, 50);
+        g.drawString("19", 320, 50);
+        g.drawString("20", 350, 50);
+    }
+}
