010b345a00fdb95093c0e3dd526eb1f10974ce8a
Nikhil <nikskashyap1206@gmail.com>
2017-10-17 14:21:02 +0200

    opdracht 2

--- src/Challenge/Opdracht_2.java
+++ src/Challenge/Opdracht_2.java
@@ -6,67 +6,73 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class Opdracht_2 extends Applet {
-    TextField tekstvak;
-    Button knop;
-    int invoer = 0;
-    String s;
 
+    Label label;
+    TextField textField;
+    Button button;
 
+    int tokyo;
+    int sydney;
+    int newYork;
+    int londen;
 
     public void init() {
-        this.setSize(350, 300);
-        tekstvak = new TextField("", 10);
-        knop = new Button("Toon de tijden");
-        knop.addActionListener(new knopListener());
+        label = new Label("Voer een uur in");
+        textField = new TextField(10);
+        button = new Button("Toon Tijden");
+
+        button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String invoer = textField.getText();
+                int input = Integer.parseInt(invoer);
+
+                tokyo = input + 7;
+
+                if (tokyo > 23)
+                    tokyo = tokyo - 24;
+
+                sydney = input + 9;
+
+                if (sydney > 23)
+                    sydney = sydney - 24;
+
+                newYork = input - 6;
+
+                if (newYork < 0)
+                    newYork = newYork + 24;
+
+                londen = input - 1;
+
+                if (londen < 0)
+                    londen = londen + 24;
 
 
-        add(tekstvak);
-        add(knop);
+                repaint();
+            }
+        });
+
+        add(label);
+        add(textField);
+        add(button);
     }
 
     @Override
     public void paint(Graphics g) {
-        int teller = 1;
+
+        setBackground(Color.black);
+
+
+        int x = 50;
         int y = 50;
-        int LO = 1;
-        int ZA = 0;
-        int To = 7;
-        int NY = 6;
+        g.setColor(Color.WHITE);
+        g.drawString("Het is in Tokyo: " + tokyo + ":00", x, y);
+        y += 20;
+        g.drawString("Het is in Sydney: " + sydney + ":00", x, y);
+        y += 20;
+        g.drawString("Het is in New York: " + newYork + ":00", x, y);
+        y += 20;
+        g.drawString("Het is in Londen: " + londen + ":00", x, y);
 
-        if (invoer > 0 && invoer < 24) {
-            while(teller <= 1) {
-
-         if (NY < 0) {
-             NY = NY + 24;
-
-         if (LO < 0) {
-             LO = LO + 24;
-
-         if (To > 23) {
-             To = To - 24;
-         }
-         }
-
-         }
-
-                g.drawString("Het is in Londen: " + (invoer-LO) + ":00"+ "uur", 70, 40);
-                g.drawString("Het is in Zuid-Afrika: " + (invoer-ZA) + ":00"+ "uur", 70, 60);
-                g.drawString("Het is in Tokyo:" + (invoer+To) + ":00" + "uur", 70, 80);
-                g.drawString("Het is in New York: " + (invoer-NY) + ":00" + "uur", 70, 100);
-
-                y += 20;
-                teller++;
-            }
-        }
-        tekstvak.setText("");
     }
-    class knopListener implements ActionListener {
-        public void actionPerformed(ActionEvent e) {
-            s = tekstvak.getText();
-            invoer = Integer.parseInt(tekstvak.getText());
-            repaint();
-
-        }
-    }
-
 }
